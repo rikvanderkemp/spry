793056ec98e50f3c35a4f36271c1d14aae9e3640
Rik van der Kemp <rik@mief.nl>
2014-12-17 20:47:47 +0100

    Added build script

--- spry.sh
+++ spry.sh
@@ -8,20 +8,52 @@ function killTasks() {
             do
                 kill $line
             done < run.lock
-    fi    
+    fi
+}
+
+function runAll() {
+    echo "Running SPRY on port 8000"
+    python main.py & 
+    jobs -p > run.lock
 }
 
 if [ "$1" = "start" ]
     then
         killTasks
-        echo "Running SPRY on port 8000"
-        python main.py & 
-
-        jobs -p > run.lock
+        runAll
         echo "All tasks run in the background, press enter if you did not return to your cli"
 elif [ "$1" = "stop" ]
     then
         killTasks
+        rm run.lock
+elif [ "$1" = "build" ]
+    then
+        if [ ! -f run.lock ];
+            then
+                runAll
+        fi
+
+        FILES=./web/templates/*
+
+        if [ -d build ]
+            then
+                rm -Rf build
+        fi
+
+        mkdir build
+
+        cp -Rf web/static build
+
+        for f in $FILES
+        do
+            if [ -f $f ];
+                then
+                  echo "Processing $f file..."
+                  wget  "http://localhost:8000/$(basename $f)" -O ./build/$(basename $f)
+            fi
+        done
+
+        echo "Build complete."
 else
     echo "Please run with start|stop"
 fi
